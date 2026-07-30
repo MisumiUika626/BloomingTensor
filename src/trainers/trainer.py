22b6ddad1758785a7ca55bf322dda7e51739c751
misumi <815759256@qq.com>
2026-07-31 03:36:09 +0800

    replace manual gradients with autograd training

--- src/trainers/trainer.py
+++ src/trainers/trainer.py
@@ -7,19 +7,4 @@ class Trainer:
         # (1)the trainer needs the loss funtion
         loss=(target-prediction)**2  # Example squared loss function
         return loss
-    def compute_gradient(self,
-                         x,
-                         prediction,
-                         target):
-        # (2)the trainer needs the gradient compuatation
-        #gradient=2*(prediction-target)
-        #return gradient
-        error = prediction-target
-
-        weight_gradient=[
-            2*error*xi
-            for xi in x
-        ]
-
-        bias_gradient=2*error
-        return {"weight": weight_gradient, "bias": bias_gradient}
+    
