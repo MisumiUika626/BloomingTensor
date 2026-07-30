66008ac80c420c93aa22b2d6b0e956f2fcab2c85
misumi <815759256@qq.com>
2026-07-31 03:34:43 +0800

    refactor SGD to update Value parameters

--- src/optimizers/sgd.py
+++ src/optimizers/sgd.py
@@ -2,23 +2,12 @@ class SGD:
     def __init__ (self, model, learning_rate):
         self.model = model
         self.learning_rate = learning_rate
-
-    def step(self, gradients):
-        #print(gradients)
-        #print(type(gradients["weight"]))
-        #print(type(gradients["weight"][0]))
-
-        weight_grads = gradients["weight"]
-        bias_grad = gradients["bias"]
-
-        self.model.weight = [
-            w - self.learning_rate * gw
-            for w,gw in zip(
-                self.model.weight,
-                weight_grads
+    def step(self):
+        for parameter in self.model.parameters():
+            parameter.data-=(
+                parameter.grad*self.learning_rate
             )
-        ]
 
-        self.model.bias -= (
-        self.learning_rate * bias_grad
-    )
+    def zero_grad(self):
+        for parameter in self.model.parameters():
+            parameter.grad = 0.0
