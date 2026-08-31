f8ea9b87faf555b68d5fb85c8c39e4e6fff2cc7b
misumi <815759256@qq.com>
2026-08-31 22:03:45 +0800

    style(autograd): format scalar engine

--- src/autograd/engine.py
+++ src/autograd/engine.py
@@ -1,91 +1,91 @@
 class Value:
-    def __init__(self,data,_children=(),_op=""):
-        self.data=data
-        self.grad=0.0 #
-        self._prev=set(_children)
-        self._op=_op
-        self._backward=lambda:None #
-    def __add__(self,other):
+    def __init__(self, data, _children=(), _op=""):
+        self.data = data
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._op = _op
+        self._backward = lambda: None
+
+    def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out=Value(self.data+other.data,
-                  (self,other),
-                  "+")
+        out = Value(self.data + other.data, (self, other), "+")
+
         def _backward():
-            self.grad+=out.grad
-            other.grad+=out.grad
-        out._backward=_backward
-        return out
-    def __mul__(self,other):
-            other = other if isinstance(other, Value) else Value(other)
-            out=Value(self.data*other.data,
-                      (self,other),
-                      "*")
-            def _backward():
-                self.grad+=out.grad*other.data
-                other.grad+=out.grad*self.data
-            out._backward=_backward
-            return out
-    def __pow__(self, exponent):
-        assert isinstance(exponent, (int, float))
-        out = Value(
-            self.data ** exponent,
-            (self,),
-            f"**{exponent}"
-    )
-        def _backward():
-            self.grad += (
-                exponent
-                * self.data ** (exponent - 1)
-                * out.grad
-        )
+            self.grad += out.grad
+            other.grad += out.grad
+
         out._backward = _backward
         return out
-    def __sub__(self,other):
-            other = other if isinstance(other, Value) else Value(other)
-            out=Value(self.data-other.data,
-                      (self,other),
-                      "-")
-            def _backward():
-                self.grad+=out.grad
-                other.grad-=out.grad
-            out._backward=_backward
-            return out
-    def __neg__(self):
-        out=Value(-self.data,
-                   (self,),
-                   f"-")
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), "*")
+
         def _backward():
-            self.grad+=-out.grad
-        out._backward=_backward
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
+
+        out._backward = _backward
         return out
-    def __radd__(self,other):
-        return self+other
-    def __rmul__(self,other):
-        return self*other 
-    def __rsub__(self,other):
-        return other+(-self)
-    def __truediv__(self, other): # self / other
+
+    def __pow__(self, exponent):
+        assert isinstance(exponent, (int, float))
+        out = Value(self.data**exponent, (self,), f"**{exponent}")
+
+        def _backward():
+            self.grad += exponent * self.data ** (exponent - 1) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __sub__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data - other.data, (self, other), "-")
+
+        def _backward():
+            self.grad += out.grad
+            other.grad -= out.grad
+
+        out._backward = _backward
+        return out
+
+    def __neg__(self):
+        out = Value(-self.data, (self,), "-")
+
+        def _backward():
+            self.grad += -out.grad
+
+        out._backward = _backward
+        return out
+
+    def __radd__(self, other):
+        return self + other
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __rsub__(self, other):
+        return other + (-self)
+
+    def __truediv__(self, other):  # self / other
         return self * other**-1
-    def __rtruediv__(self, other): # other / self
+
+    def __rtruediv__(self, other):  # other / self
         return other * self**-1
-    
+
     def backward(self):
-        topo=[]
-        visited=set()
+        topo = []
+        visited = set()
+
         def build_topo(v):
-            #拓扑排序建立计算图
+            # 拓扑排序建立计算图
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
                     build_topo(child)
                 topo.append(v)
+
         build_topo(self)
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
-              
-              
-              
-              
-         
-
